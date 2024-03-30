d180f69d01c5610f66ff5238984f48f2b0ff3935
flaviohfsilva <flaviohenriquefh18.work@gmail.com>
2024-03-30 18:22:21 -0300

    ✨ Adição do carrosel da home page, imagens do carrosel, arquivo de interfaces e ajuste do ajudar-container na responsividade

---
+++ luto-parental/src/app/interfaces.ts
@@ -0,0 +1,6 @@
+export interface CarouselHome {
+  imgSrc: string;
+  imgAlt: string;
+  title: string;
+  subtitle: string;
+}
--- luto-parental/src/app/pages/landing-page/landing-page.component.ts
+++ luto-parental/src/app/pages/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { CarouselHome } from 'src/app/interfaces';
 
 @Component({
   selector: 'app-landing-page',
@@ -6,5 +7,90 @@ import { Component } from '@angular/core';
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent {
+
+  selectedIndex: number = 0;
+  sliders: boolean = true;
+  controls: boolean = true;
+  carouselImages: boolean = false;
+  carouselAutomatic: boolean = true;
+  textCarousel: boolean = true;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.verificarTamanhoTela();
+
+    if(this.carouselAutomatic){
+      this.carouselAutomatico();
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  // Verifica o tamanho da tela
+  verTamanho(event: Event){
+    this.verificarTamanhoTela();
+  }
+
+  //
+  images: CarouselHome[] = [
+      {
+        imgSrc: '../../../assets/carousel/voce-nao-esta-sozinha.png',
+        imgAlt: 'Você não está sozinha',
+        title: 'Você não está sozinha!',
+        subtitle: 'Conte com atendimento emergencial gratuito com psicólogas voluntárias e especializadas em luto parental.'
+    },
+    {
+      imgSrc: '../../../assets/carousel/mae-natimorto.png',
+      imgAlt: 'Mãe de natimorto deve ter direitos',
+      title: 'Mãe de natimorto deve ter direito a ala separada na maternidade.',
+      subtitle: 'Mães de natimortos tem direito a acomodação em ala ou leitos separados das demais gestantes.'
+    }
+  ]
+
+  passarSliderCarousel(index: number){
+    this.selectedIndex = index;
+  }
+
+  voltarCarousel(){
+    if(this.selectedIndex === 0){
+      this.selectedIndex = this.images.length - 1;
+    } else {
+      this.selectedIndex--;
+    }
+  }
+
+  avancarCarousel(){
+    if(this.selectedIndex === this.images.length - 1){
+      this.selectedIndex = 0;
+    } else {
+      this.selectedIndex++;
+    }
+  }
+
+  carouselAutomatico(){
+    setInterval(() => {
+      this.avancarCarousel();
+    }, 8000);
+  }
+
+  verificarTamanhoTela(){
+  if (this.carouselImages = window.innerWidth <= 767) {
+      const images: CarouselHome[] = [
+        {
+          imgSrc: '../../../assets/carousel/voce nao esta sozinha-responsiva.png',
+          imgAlt: 'Você não está sozinha',
+          title: 'Você não está sozinha!',
+          subtitle: 'Conte com atendimento emergencial gratuito com psicólogas voluntárias e especializadas em luto parental.'
+        },
+        {
+          imgSrc: '../../../assets/carousel/mae natimorto-responsiva.png',
+          imgAlt: 'Mãe de natimorto deve ter direitos',
+          title: 'Mãe de natimorto deve ter direito a ala separada na maternidade.',
+          subtitle: 'Mães de natimortos tem direito a acomodação em ala ou leitos separados das demais gestantes.'
+        }
+      ]
+    }
+  }
+
 
 }
