a96f45147d7e2365a82a31f223ff98f00be69f2c
Gabriela Diniz <gadisa10@outlook.com>
2024-04-03 23:08:33 -0300

    📱 style: inclusão do background e início da responsividade com ele

--- luto-parental/src/app/pages/noticias/noticias.component.ts
+++ luto-parental/src/app/pages/noticias/noticias.component.ts
@@ -37,36 +37,71 @@ export class NoticiasComponent {
       imagem: 'noticia-2',
       data: '10/10/2017',
       categoria: 'ARTIGO',
-      titulo: 'POLÍTICA DE HUMANIZAÇÃO DO LULUTO PARENTAL E CONSTRUÇÃO IDENTITÁRIA: COMPREENDENDO O PROCESSO APÓS A PERDA DO FILHOTO PARENTAL',
-      texto: "A morte de um filho é um fato inesperado pelos pais, geralmente é visto como algo contrário à natureza, incitando um luto muito particular. As manifestações desse luto são diversas...",
+      titulo:
+        'POLÍTICA DE HUMANIZAÇÃO DO LULUTO PARENTAL E CONSTRUÇÃO IDENTITÁRIA: COMPREENDENDO O PROCESSO APÓS A PERDA DO FILHOTO PARENTAL',
+      texto:
+        'A morte de um filho é um fato inesperado pelos pais, geralmente é visto como algo contrário à natureza, incitando um luto muito particular. As manifestações desse luto são diversas...',
     },
     {
       imagem: 'noticia-3',
       data: '10/10/2017',
       categoria: 'ARTIGO',
-      titulo: 'SAIBA OS PRINCIPAIS DIREITOS RELACIONADOS À SAÚDE DAS FAMÍLIAS ENLUTADAS ',
-      texto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      titulo:
+        'SAIBA OS PRINCIPAIS DIREITOS RELACIONADOS À SAÚDE DAS FAMÍLIAS ENLUTADAS ',
+      texto:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     },
     {
       imagem: 'noticia-4',
       data: '10/10/2017',
       categoria: 'NOTÍCIA',
       titulo: 'O SILÊNCIO EM TORNO DO LUTO PARENTAL DENTRO DAS EMPRESAS.',
-      texto: 'O luto parental é um tema frequentemente negligenciado nas empresas, com os departamentos de Recursos Humanos mostrando uma compreensão limitada de como proteger os direitos dos funcionários...'
+      texto:
+        'O luto parental é um tema frequentemente negligenciado nas empresas, com os departamentos de Recursos Humanos mostrando uma compreensão limitada de como proteger os direitos dos funcionários...',
     },
     {
       imagem: 'noticia-5',
       data: '10/10/2017',
       categoria: 'NOTÍCIA',
-      titulo: 'LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE.',
-      texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...'
+      titulo:
+        'LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE.',
+      texto:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...',
     },
     {
       imagem: 'noticia-6',
       data: '10/10/2017',
       categoria: 'ARTIGO',
-      titulo: 'COMO VIVER O LUTO DE MANEIRA MAIS SAUDÁVEL E PRESERVAR A SUA SAÚDE MENTAL',
-      texto: 'A saúde mental corresponde muito mais do que somente à ausência de doenças mentais. De acordo com a Organização Mundial da Saúde (OMS), saúde mental é um estado de bem-estar no qual o indivíduo é capaz de usar...'
+      titulo:
+        'COMO VIVER O LUTO DE MANEIRA MAIS SAUDÁVEL E PRESERVAR A SUA SAÚDE MENTAL',
+      texto:
+        'A saúde mental corresponde muito mais do que somente à ausência de doenças mentais. De acordo com a Organização Mundial da Saúde (OMS), saúde mental é um estado de bem-estar no qual o indivíduo é capaz de usar...',
+    },
+  ];
+
+  public categorias = [
+    {
+      categoria: 'NOTÍCIA',
+    },
+    {
+      categoria: 'ARTIGO'
     }
   ];
+
+  /**
+   * Retorna a cor de cada categoria no DOM.
+   * 
+   * @param categoria categorias disponíveis de notícias
+   * @returns a cor da categoria
+   */
+  public visualizarCorCategoria(categoria: string): string {
+    switch(categoria) {
+      case 'NOTÍCIA':
+        return '#D9B2A9';
+      case 'ARTIGO':
+        return '#60748D';
+      default:
+        return ''
+    }
+  }
 }
